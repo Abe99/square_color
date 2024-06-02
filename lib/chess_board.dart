5bd13a72d555c543ce9ab8bf69f0995549be3e17
Abe99 <i.mqami@gmail.com>
2024-06-02 23:01:40 +0100

    added squares/color Map

--- lib/chess_board.dart
+++ lib/chess_board.dart
@@ -1,1 +1,26 @@
-//TODO: generate the chess board
+import 'dart:math';
+
+
+Map<String, String> createChessboard() {
+  var chessboard = <String, String>{};
+  var columns = 'abcdefgh';
+  var rows = '12345678';
+  for (var col in columns.runes) {
+    for (var row in rows.runes) {
+      var square = String.fromCharCode(col) + String.fromCharCode(row);
+      if ((columns.indexOf(String.fromCharCode(col)) +
+              rows.indexOf(String.fromCharCode(row))) %
+          2 ==
+          0) {
+        chessboard[square] = 'b';
+      } else {
+        chessboard[square] = 'w';
+      }
+    }
+  }
+  return chessboard;
+}
+Map <String, String> chessBoard = createChessboard();
+
+var squares = chessBoard.keys.toList();
+var randomSquare = squares[Random().nextInt(squares.length)];
--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,79 @@
-//TODO: basic scaffold
+//TODO: base scaffold
+import 'chess_board.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const GuessTheColor());
+}
+
+class GuessTheColor extends StatelessWidget {
+  const GuessTheColor({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        home: Scaffold(
+      backgroundColor: Colors.amber.shade300,
+      body: SafeArea(
+        child: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 10.0),
+          child: QuizPage(),
+        ),
+      ),
+    ));
+  }
+}
+
+class QuizPage extends StatefulWidget {
+  const QuizPage({super.key});
+
+  @override
+  State<QuizPage> createState() => _QuizPageState();
+}
+
+class _QuizPageState extends State<QuizPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Expanded(
+            child: Center(
+          child: Text(
+            '$randomSquare',
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              fontSize: 25.0,
+              color: Colors.amber.shade900,
+            ),
+          ),
+        )),
+        Expanded(
+          child: TextButton(
+            
+            onPressed: () {},
+            child: Text(
+              'White',
+              style:
+               TextStyle(
+                fontSize: 20.0,
+                color: Colors.purple,
+              ),
+            ),
+          ),
+        ),
+        Expanded(
+          child: TextButton(
+            onPressed: () {},
+            child: Text(
+              'Black',
+              style: TextStyle(
+                fontSize: 20.0,
+                color: Colors.teal,
+              ),
+            ),
+          ),
+        )
+      ],
+    );
+  }
+}
